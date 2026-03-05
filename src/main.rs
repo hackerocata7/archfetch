7c083fedc73ee8da0be7bc971833f7ee33e89234
hackerocata7 <a.liam.jurado@iesmediterrania.cat>
2026-03-05 19:47:46 +0100

    Finish os and strart hostname

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+extern crate os_release;
+
+use os_release::OsRelease;
+
 fn compiletext(acii: Vec<String>, details: Vec<String>) -> String {
+    String::new()
+}
+
+fn get_info() -> Vec<String> {
+    let mut retv = Vec::new();
+    let OSR = OsRelease::new().expect("Cant parse os release");
+
+    let release = OSR.pretty_name;
+    let version = OSR.version;
+    let os = format!("{release} {version}");
+    retv.push(os);
+
+    let hostname = hostname::get().expect("Failed to parse hostname.");
+    retv.push(hostname);
+
+
+    retv
+
+
 
 }
 
 fn main() {
-
+    let v = get_info();
+    println!("{}", v[0]);
+    println!("{}", v[1])
 }
