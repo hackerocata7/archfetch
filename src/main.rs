f2c28515b202b17ed052b3cdbe4141fcf438e6d0
hackerocata7 <a.liam.jurado@iesmediterrania.cat>
2026-03-07 23:45:32 +0100

    Add Nerd Fonts!

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use sysinfo::System;
 use bytesize::ByteSize;
 use get_shell::get_shell_name;
 use os_release::OsRelease;
-use std::ffi::OsString;
 use std::fs;
 use std::env;
 
@@ -33,39 +32,39 @@ fn do_ascii() -> Vec<String> {
 fn get_kern() -> String {
     let f = fs::read_to_string("/proc/version").expect("Couldnt get kernel version");
     let i = f.split(" ").collect::<Vec<&str>>();
-    format!("{} {} {}", i[0], i[1], i[2])
+    format!(" {} {} {}", i[0], i[1], i[2])
 }
 
 fn get_info() -> Vec<String> {
     let mut retv = Vec::new();
-    let OSR = OsRelease::new().expect("Cant parse os release");
+    let osr = OsRelease::new().expect("Cant parse os release");
 
-    let release = OSR.pretty_name;
-    let version = OSR.version;
-    let os = format!("{release} {version}");
+    let release = osr.pretty_name;
+    let version = osr.version;
+    let os = format!("󰍹 {release} {version}");
     retv.push(os);
 
 //    let hostname = hostname::get().expect("Failed to parse hostname.").into_string().expect("Error parseing to string hostname.");
 //    retv.push(hostname);
 
     let host = fs::read_to_string("/sys/devices/virtual/dmi/id/product_family").expect("Could not read host").trim().to_string();
-    retv.push(host);
+    retv.push(format!("󰌢 {host}"));
 
     let kernel = get_kern();
     retv.push(kernel);
 
     let shell = get_shell_name().expect("Could not parse shell name.");
-    retv.push(shell);
+    retv.push(format!(" {shell}"));
 
     let wm = env::var("XDG_CURRENT_DESKTOP").expect("Couldnt retrive WM");
-    retv.push(wm);
+    retv.push(format!(" {wm}"));
 
     let mut sys = System::new_all();
     sys.refresh_all();
     // Ignore the double .to_string();
     let ramgb = ByteSize::b(sys.used_memory()).as_gib().to_string()[..4].to_string();
     let ramgbtot = ByteSize::b(sys.total_memory()).as_gib().to_string()[..4].to_string();
-    retv.push(format!("{ramgb} GB / {ramgbtot} GB"));
+    retv.push(format!(" {ramgb} GB / {ramgbtot} GB"));
 
     retv
 
