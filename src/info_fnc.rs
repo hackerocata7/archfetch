72b32a37840cc73ce7cb642a2da1bc96554f5c96
hackerocata7 <a.liam.jurado@iesmediterrania.cat>
2026-03-10 20:32:23 +0100

    Configuration file.

--- src/info_fnc.rs
+++ src/info_fnc.rs
@@ -1,9 +1,15 @@
 use super::{get_system, get_host, get_kern, get_shell, get_wm, get_ram, get_disk, get_cpu};
 use ansi_term::Colour;
 
+// Define some variables that are used in lib.rs and here.
+pub const COLOR_ICON: &str = "";
+pub const SEPARATION: u8 = 2;
+
 pub fn get_info() -> Vec<String> {
+    // Create retv to later return it.
     let mut retv = Vec::new();
-        
+
+    // To add some text, push to retv
     retv.push(format!("󰍹 {}", get_system()));
 
     retv.push(format!("󰌢 {}", get_host()));
@@ -20,8 +26,9 @@ pub fn get_info() -> Vec<String> {
 
     retv.push(format!(" {}", get_cpu()));
     
-    retv.push(format!(" {} {} {} {} {} {} {} {} ", Colour::Black.paint(""), Colour::Red.paint(""), Colour::Green.paint(""), Colour::Yellow.paint(""), Colour::Blue.paint(""), Colour::Purple.paint(""), Colour::Cyan.paint(""), Colour::White.paint("")));
+    retv.push(format!(" {} {} {} {} {} {} {} {} ", Colour::Black.paint(COLOR_ICON), Colour::Red.paint(COLOR_ICON), Colour::Green.paint(COLOR_ICON), Colour::Yellow.paint(COLOR_ICON), Colour::Blue.paint(COLOR_ICON), Colour::Purple.paint(COLOR_ICON), Colour::Cyan.paint(COLOR_ICON), Colour::White.paint(COLOR_ICON)));
     
+    // Return retv so it can be used upstream.
     retv
 
 
